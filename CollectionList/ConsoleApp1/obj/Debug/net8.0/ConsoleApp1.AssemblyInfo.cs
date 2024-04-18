848544be88a022106c2a68028b2e206bcf5dce12
Roberts Eglītis <120631572+G4AR4@users.noreply.github.com>
2024-04-18 09:24:13 +0000

    nothing

--- CollectionList/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ CollectionList/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1b2e457664de567463f0834c63fda5635273d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe933feb64f859ed882be85d1dd2716c309e0cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
